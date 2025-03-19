6dc34bdb3f4fb51dfb0cf259e2c9691bdd05bc58
Doug Anderson444 <douganderson444@gmail.com>
2025-03-19 14:15:44 -0300

    use features

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
 //! Multicrates library re-exporting all member crates.
 
-// Re-export all member crates
+// Re-export all member crates with feature flags
+#[cfg(feature = "multibase")]
 pub use multibase;
+
+#[cfg(feature = "multicodec")]
 pub use multicodec;
+
+#[cfg(feature = "multihash")]
 pub use multihash;
+
+#[cfg(feature = "multisig")]
 pub use multisig;
+
+#[cfg(feature = "multitrait")]
 pub use multitrait;
+
+#[cfg(feature = "multiutil")]
 pub use multiutil;
 
 /// Version of the crate
